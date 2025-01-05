5fdf0864d53b4acb07f67f98c1f9c115376c3061
jokw1228 <jokw1228@naver.com>
2025-01-05 11:50:55 +0900

    [20250105] UI 명세서대로 파일만 추가

---
+++ Assets/Scripts/UI/UIEvents.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEvents : MonoBehaviour
+{
+    // 보드 관련
+    public static event Action<Cell[,]> OnBoardChanged;
+    public static event Action<Block, Vector2Int> OnBlockMoved;
+    public static event Action<BlockData, Vector2Int> OnBlockPlaced;
+    public static event Action<MatchType, List<Vector2Int>> OnMatchCompleted;
+
+    // 덱 관련
+    public static event Action<List<BlockData>> OnDeckChanged;
+    public static event Action OnRerollRequested;
+
+    // 점수/진행 관련
+    public static event Action<int> OnScoreChanged;
+    public static event Action<int> OnGoldChanged;
+    public static event Action<StageProgress> OnProgressUpdated;
+
+    // 상점 관련
+    public static event Action<ItemData> OnItemSelected;
+    public static event Action<BlockData> OnUpgradeSelected;
+}

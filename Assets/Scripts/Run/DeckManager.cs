532aa97eff4a4ef8ebc121a5d30d06582aa0b244
KangHee <rkdgml0820@naver.com>
2025-01-06 20:51:51 +0900

    DeckManager 일부 구현

--- Assets/Scripts/Run/DeckManager.cs
+++ Assets/Scripts/Run/DeckManager.cs
@@ -1,23 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DeckManager : MonoBehaviour
 {
-    private List<BlockData> deck;
-    private List<BlockData> discardPile;
+    private List<Block> deck;
+    private List<Block> discardPile;
     private RunData runData;
+
+    private int idCount;
 
     // 덱 초기화
     public void Initialize(RunData data)
     {
-
+        deck = InstantiateBlocks(data.availableBlocks);
+        discardPile = new List<Block>();
+        runData = data;
+        idCount = 0;
     }
 
     // 블록 뽑기
-    public BlockData DrawBlock()
+    public Block DrawBlock()
     {
-        return null;
+        if (deck.Count == 0)
+        {
+            Debug.Log("드로우 시도: 덱에 블록 없음");
+            return null;
+        }
+
+        // TEST
+        int randomIdx = Random.Range(0, deck.Count);
+        Block block = deck[randomIdx];
+        deck.RemoveAt(randomIdx);
+
+        int rotateCount = Random.Range(0, 4);
+        for (int i = 0; i < rotateCount; i++)
+        {
+            block.RotateShape();
+        }
+
+        return block;
     }
 
     // 덱 셔플
@@ -48,5 +72,24 @@ public class DeckManager : MonoBehaviour
     public void RemoveBlock(BlockData block)
     {
 
+    }
+
+    private List<Block> InstantiateBlocks(List<BlockData> blockDatas)
+    {
+        List<Block> blocks = new List<Block>();
+
+        foreach (BlockData blockData in blockDatas)
+        {
+            GameObject blockObject = Instantiate(blockData.prefab);
+            Block block = blockObject.GetComponent<Block>();
+            block.Initialize(blockData, idCount);
+            idCount++;
+            blocks.Add(block);
+
+            // TEST
+            blockObject.transform.position = new Vector3(10, 10);
+        }
+
+        return blocks;
     }
 }

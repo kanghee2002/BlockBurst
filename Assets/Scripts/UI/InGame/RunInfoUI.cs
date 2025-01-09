50ad375a84a530623726e47f1e3877aa3f99641b
jokw1228 <jokw1228@naver.com>
2025-01-09 16:59:35 +0900

    DOTween 에셋 추가

--- Assets/Scripts/UI/InGame/RunInfoUI.cs
+++ Assets/Scripts/UI/InGame/RunInfoUI.cs
@@ -1,20 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class RunInfoUI : MonoBehaviour
 {
+    [SerializeField] private GameObject runInfoUI;
+    [SerializeField] private RectTransform rectTransform;
 
     [SerializeField] private GameUIManager gameUIManager;
 
+    private const float centerPositionY = 0;
+    private const float outsidePositionY = -1024;
+
+    private const float duration = 0.2f;
+
     public void OpenRunInfoUI()
     {
-        Debug.Log("RunInfoUI가 화면 밑에서 위로 올라옴");
+        runInfoUI.SetActive(true);
+        rectTransform.DOAnchorPosY(centerPositionY, duration)
+            .SetEase(Ease.OutSine);
     }
 
     public void CloseRunInfoUI()
     {
         Debug.Log("RunInfoUI가 화면 중앙에서 아래로 내려감");
+        rectTransform.DOAnchorPosY(outsidePositionY, duration)
+            .SetEase(Ease.OutSine)
+            .OnComplete(() =>
+             {
+                 runInfoUI.SetActive(false);
+             });
     }
 
     public void OnRunInfoBackButtonUIPressed()

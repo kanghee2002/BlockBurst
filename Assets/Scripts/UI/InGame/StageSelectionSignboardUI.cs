958057a32a381109e6151632da976da99775c57f
YeoJoon <joyyoj1@naver.com>
2025-01-15 18:40:07 +0900

    [20250115] chapter-stage

--- Assets/Scripts/UI/InGame/StageSelectionSignboardUI.cs
+++ Assets/Scripts/UI/InGame/StageSelectionSignboardUI.cs
@@ -2,9 +2,11 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class StageSelectionSignboardUI : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI stageSelectionSignboardText;
     [SerializeField] private GameObject stageSelectionSignboardUI;
     [SerializeField] private RectTransform rectTransform;
     // inside anchored position = (300,256)
@@ -26,5 +28,11 @@ public class StageSelectionSignboardUI : MonoBehaviour
             {
                 stageSelectionSignboardUI.SetActive(false);
             });
+    }
+    
+    public void Initialize(int currentChapterIndex, int currentStageIndex)
+    {
+        string text = currentChapterIndex + " - " + currentStageIndex + "\n" + "스테이지를 선택하세요";
+        stageSelectionSignboardText.text = text;
     }
 }

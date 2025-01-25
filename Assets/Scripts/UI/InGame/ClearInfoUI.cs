3d0bddad7fb68e0e8b528da5b0a34d09600ad48c
jokw1228 <jokw1228@naver.com>
2025-01-26 00:43:49 +0900

    ClearInfoUI 게임 오브젝트 추가

---
+++ Assets/Scripts/UI/InGame/ClearInfoUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearInfoUI : MonoBehaviour
+{
+    private RectTransform rectTransform;
+
+    private const float insidePositionY = 0;
+    private const float outsidePositionY = 1536;
+    private const float duration = 0.4f;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+    void Start()
+    {
+        OpenClearInfoUI();
+    }
+
+    public void OpenClearInfoUI()
+    {
+        gameObject.SetActive(true);
+        UIUtils.OpenUI(rectTransform, "Y", insidePositionY, duration);
+    }
+
+    public void CloseClearInfoUI()
+    {
+        UIUtils.CloseUI(rectTransform, "Y", insidePositionY, outsidePositionY, duration);
+    }
+}

ff0738cd3904ba758a94b5689448b45ec1f6b886
kanghee2002 <rkdgml0820@naver.com>
2025-07-11 15:50:43 +0900

    모두 해금 텍스트 버그 수정

--- Assets/Scripts/UI/InGame/PlayerDataBoardUI.cs
+++ Assets/Scripts/UI/InGame/PlayerDataBoardUI.cs
@@ -9,6 +9,8 @@ public class PlayerDataBoardUI : MonoBehaviour
     [SerializeField] private StatisticsContainerUI statisticsContainerUI;
     [SerializeField] private UnlockContainerUI unlockContainerUI;
 
+    [SerializeField] private GameObject UnlockAllLayout;
+
     private RectTransform rectTransform;
 
     private const float insidePositionX = 0;
@@ -37,11 +39,42 @@ public class PlayerDataBoardUI : MonoBehaviour
         gameObject.SetActive(true);
         popupBlurImage.OpenPopupBlurImage(new Color(0.0f, 0.0f, 0.0f, 0.9f));
         UIUtils.OpenUI(rectTransform, "Y", insidePositionX, duration);
+
+        UnlockAllLayout.SetActive(true);
     }
 
     public void ClosePlayerDataBoardUI()
     {
         popupBlurImage.ClosePopupBlurImage();
         UIUtils.CloseUI(rectTransform, "Y", insidePositionX, outsidePositionOffsetX, duration);
+
+        UnlockAllLayout.SetActive(false);
     }
+
+
+    // 평가용
+    public void OnUnlockAllButtonUIPressed()
+    {
+        UnlockInfo[] unlockInfoTemplates = Resources.LoadAll<UnlockInfo>("ScriptableObjects/UnlockInfo");
+
+        foreach (UnlockInfo unlockInfo in unlockInfoTemplates)
+        {
+            UnlockManager.instance.Unlock(unlockInfo, unlockInfo.targetName);
+        }
+
+        foreach (DeckType deckType in Enums.GetEnumArray<DeckType>())
+        {
+            for (int i = 0; i < DeckInfo.MAX_LEVEL + 1; i++)
+            {
+                DataManager.instance.UpdateDeckLevel(deckType, i);
+            }
+        }
+
+        UnlockAllLayout.SetActive(false);
+
+        GameObject.Find("UnlockNotificationUI").SetActive(false);
+
+        GameUIManager.instance.OnPopupBlurUIPressed();
+    }
+
 }

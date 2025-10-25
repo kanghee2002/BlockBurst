9d40a009aad9af58e63e95d5824ad1cdac635636
kanghee2002 <rkdgml0820@naver.com>
2025-10-25 10:37:22 +0900

    남은 블록 텍스트 경고 애니메이션 구현

--- Assets/Scripts/UI/InGame/DeckButtonUI.cs
+++ Assets/Scripts/UI/InGame/DeckButtonUI.cs
@@ -1,12 +1,14 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class DeckButtonUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI deckCountText;
+
     public TextMeshProUGUI deckTitleText;
     public Image bg;
 
@@ -15,6 +17,16 @@ public class DeckButtonUI : MonoBehaviour
         // deckCountText.text = deckCount.ToString() + "/" + maxDeckCount.ToString();
         deckCountText.text = deckCount.ToString();
         UIUtils.BounceText(deckCountText.transform);
+
+        if (deckCount <= 10)
+        {
+            int scalar = 10 - deckCount;
+            Color color = new Color(200f / 255f, 40f/ 255f, 40f / 255f);
+            Color scaledColor = new Color(color.r + (1f - color.r) * scalar / 10f, color.g * deckCount / 10f, color.b * deckCount / 10f);
+            Color resultColor = new Color(scaledColor.r, scaledColor.g, scaledColor.b, 1f);
+            deckCountText.DOColor(resultColor, 0.5f)
+                .SetEase(Ease.OutQuad);
+        }
     }
 
     public void SetColorOfUI(Color uiColor, Color textColor)
@@ -24,7 +36,5 @@ public class DeckButtonUI : MonoBehaviour
         UIUtils.SetTextColorByScalar(deckCountText, textColor, 1f);
         UIUtils.SetTextColorByScalar(deckTitleText, textColor, 1f);
         GetComponent<ButtonUI>().SetUIColor(uiDim);
-
-        
     }
 }
